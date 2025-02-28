588dffd8640ba79c68250ce97cdae477d0c6299d
Govind Choudhary <govind2220000@gmail.com>
2025-02-28 15:08:40 +0530

    Added changes for deployment using github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/pocket-notes-app/',
   plugins: [react()],
   server: {
     allowedHosts: [".ngrok-free.app"],
